7e428fc8b672beaa98c8407a00c5f3be2ff32e22
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-03-07 21:38:30 +0700

    Logger

---
+++ p031-logger-01.rb
@@ -0,0 +1,15 @@
+require "logger"
+$LOG = Logger.new('log_file.log', 'monthly')
+def divide(numerator, denominator)
+  $LOG.debug("Numerator: #{numerator}, denominator #{denominator}")
+  begin 
+    result = numerator / denominator
+  rescue Exception => e 
+    $LOG.error "Error in division! : #{e}"
+    result = nil
+  end
+  return result
+end
+
+puts divide(10, 2)
+puts divide(2, 0)
